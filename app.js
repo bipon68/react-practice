779bea1e50d9c7d3251b4ef3fac2e8eb930a16a1
bipon68 <bipon68@users.noreply.github.com>
2020-07-12 04:23:49 +0600

    create element and render dom

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('connected')
+console.log('React : ', React)
+console.log('ReactDOM : ', ReactDOM);
+
+// const anotherElement = React.createElement('div', {class: 'container', element})
+
+const element = React.createElement('h2', {id: 'hello'}, 'Hello World');
+ReactDOM.render(element, document.getElementById('root'))
+console.log(element)
