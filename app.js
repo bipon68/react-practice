cf4d69f195477d4f39e98cb8217d6d3c278fb1e3
bipon68 <bipon68@users.noreply.github.com>
2020-07-12 05:08:52 +0600

    parcel added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ console.log('ReactDOM : ', ReactDOM);
 const element = React.createElement('div', null, 
     React.createElement('h2', {id: 'hello'}, 'Hello World')
 )
+
+// const element = <h2 id="hello">Hello World</h2>;
 const anotherElement = React.createElement('div', {className: 'container'}, element)
 
 ReactDOM.render(anotherElement, document.getElementById('root'))
