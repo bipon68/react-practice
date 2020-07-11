ad26681452c2b7ddd35ea1c44005c7dca6d2c55e
bipon68 <bipon68@users.noreply.github.com>
2020-07-12 04:33:56 +0600

    passing null as a props into element

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log('React : ', React)
 console.log('ReactDOM : ', ReactDOM);
 
-const element = React.createElement('h2', {id: 'hello'}, 'Hello World');
-
+const element = React.createElement('div', null, 
+    React.createElement('h2', {id: 'hello'}, 'Hello World')
+)
 const anotherElement = React.createElement('div', {className: 'container'}, element)
 
 ReactDOM.render(anotherElement, document.getElementById('root'))
