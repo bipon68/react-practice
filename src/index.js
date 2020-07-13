537bbca6d75b28e67ec31e56043d4af29929d9bc
bipon68 <bipon68@users.noreply.github.com>
2020-07-13 18:42:06 +0600

    ES6 modular system
    default export , named export

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
+import add, {subtract, multiply, num1, num2} from './playground/math';
+
+console.log(add(num1,num2))
+console.log(subtract(num1,num2))
+console.log(multiply(num1,num2))
+
 
 //rendering DOM
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/playground/math.js
@@ -0,0 +1,19 @@
+const num1 = 6;
+const num2 = 4;
+
+function add(num1, num2){;
+    return num1 + num2
+}
+
+function subtract(num1, num2){;
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+export {add as default, subtract, multiply, num1, num2};
+
+// default export 
+// named
