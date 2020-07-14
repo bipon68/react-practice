0930aaa69ef0f877222ab3fc7d72490d57af1e48
bipon68 <bipon68@users.noreply.github.com>
2020-07-14 11:12:44 +0600

    state

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,26 @@ console.log("ReactDOM : ", ReactDOM);
 //Typechecking With PropTypes - https://reactjs.org/docs/typechecking-with-proptypes.html
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      count: 0,
+      counting: false
+    }
+  }
+
   render() {
+    // let counted;
+    // if(this.state.counting ){
+    //   counted = 'You are counting. Hurry'
+    // }else{
+    //   counted = 'You are not counting!'
+    // }
     return (
       <div>
+        <p>{this.state.counting ? 'You are counting. Hurry': 'You are not counting!'}</p>
+        <p>Count : {this.state.count}</p>
         <Example name="Bipon" />
         <Parent country="Bangladesh"/>
         <Friends friends={['Bipon', 'Ankon', 'Mahfuz', 'Shazol']}/>
