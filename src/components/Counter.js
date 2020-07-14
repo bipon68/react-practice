f863449a9ab4453787f2c8c6f8bc8ca3a2ee243d
bipon68 <bipon68@users.noreply.github.com>
2020-07-14 15:37:28 +0600

    passing value with event Reference

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,6 +7,9 @@ class Counter extends Component {
           count: this.props.count,
           counting: false
         }
+        // this.handleIncrement = this.handleIncrement.bind(this)
+        this.handleDecrement = this.handleDecrement.bind(this)
+        this.handleReset = this.handleReset.bind(this)
         // this.makeTimer()
         // console.log(this.state)
       }
@@ -19,25 +22,28 @@ class Counter extends Component {
       //   }, 1000);
       // }
     
-      handleIncrement = () => {
+      handleIncrement = (number) => {
         console.log(this)
+        this.setState((prevState) => ({
+            count: prevState.count + number
+        }))
         // changing state or update state based on existing state
         // this.setState({
         //   count: this.state.count + 1,
         //   counting: true
         // })
         // if any setState update or change depend on other setState
-        this.setState((prevState) => ({ 
-          count: prevState.count + 1,
-          counting: true
-        }))
-        this.setState((prevState) => ({ 
-          count: prevState.count + 1,
+        // this.setState((prevState) => ({ 
+        //   count: prevState.count + 1,
+        //   counting: true
+        // }))
+        // this.setState((prevState) => ({ 
+        //   count: prevState.count + 1,
     
-        }))
-        this.setState((prevState) => ({ 
-          count: prevState.count + 1,
-        }))
+        // }))
+        // this.setState((prevState) => ({ 
+        //   count: prevState.count + 1,
+        // }))
         // this.setState((prevState) => {
         //   return{
         //     count: prevState.count + 1,
@@ -45,6 +51,18 @@ class Counter extends Component {
         //   } 
         // })
       }
+      handleDecrement = () => {
+          this.setState((prevState) => ({
+            count: prevState.count - 1
+          }))
+      }
+
+      handleReset(){
+        this.setState({
+            count: 0
+        })
+      }
+
       render() {
         // let counted;
         // if(this.state.counting ){
@@ -54,7 +72,9 @@ class Counter extends Component {
         // }
         return (
           <div>
-            <button onClick={this.handleIncrement}>Increment</button>
+            <button onClick={this.handleIncrement.bind(this, 5)}>Increment</button>
+            <button onClick={this.handleDecrement}>Decrement</button>
+            <button onClick={this.handleReset}>Reset</button>
             <p>{this.state.counting ? 'You are counting. Hurry': 'You are not counting!'}</p>
              <p>Count : {this.state.count}</p>
           </div>
