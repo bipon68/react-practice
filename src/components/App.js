72e65cd7f14cf0687da37046b8bda00f10c4c524
bipon68 <bipon68@users.noreply.github.com>
2020-07-15 16:54:15 +0600

    #2 initial component create

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 // import Parent from './Parent';
 // import Friends from './Friends';
 
+import Contacts from "./Contacts";
+import ContactForm from "./ContactForm";
+
 import Counter from "./Counter";
 
 console.log("React : ", React);
@@ -11,10 +14,17 @@ console.log("ReactDOM : ", ReactDOM);
 
 //Typechecking With PropTypes - https://reactjs.org/docs/typechecking-with-proptypes.html
 
-const App = () => {
-  return(
-    <Counter count={10} />
-  )
+class App extends Component {
+  render(){
+    return(
+      <React.Fragment>
+          <h3>Our app works</h3>
+          <ContactForm />
+          <Contacts />
+      </React.Fragment>
+      // <Counter count={10} />
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ContactForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class ContactForm extends Component {
+    render() {
+        return (
+            <div>
+                <p>Contact Form</p>
+            </div>
+        )
+    }
+}
+
+export default ContactForm;
