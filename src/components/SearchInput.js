c552bfadedbb75e472a06017e15bd26ac669c886
bipon68 <bipon68@users.noreply.github.com>
2020-08-02 21:32:21 +0600

    search ui

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -4,7 +4,23 @@ import React, { Component } from 'react'
     render() {
         return (
             <div>
-                Search
+            <nav>
+            <div className='nav-wrapper white'>
+                <form>
+                <div className='input-field'>
+                    <input
+                        id='search'
+                        type='search'
+                        placeholder='Search...'
+                    />
+                    <label className='label-icon' htmlFor='search'>
+                    <i className='material-icons'>search</i>
+                    </label>
+                    <i className='material-icons'>close</i>
+                </div>
+                </form>
+            </div>
+            </nav>
             </div>
         )
     }
