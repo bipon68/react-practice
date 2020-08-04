7511f69c95dc7536fbbfefb2d35cd3538bed0b00
bipon68 <bipon68@users.noreply.github.com>
2020-08-04 23:24:23 +0600

    navlink and link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                <nav>
-                    <div className="nav-wrapper spacing">
-                        <a href="#" class="brand-logo">Contact App</a>
-                        <ul id="nav-mobile" class="right hide-on-med-and-down">
-                            <li><a href="#!">Home</a></li>
-                            <li><a href="#!">About</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        )
-    }
-}
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper spacing'>
+          <NavLink
+            to='/'
+            exact
+            className='brand-logo'
+            activeClassName='nav-active'
+          >
+            ContactApp
+          </NavLink>
 
+          <ul id='nav-mobile' className='right'>
+            <li>
+              <NavLink to='/' exact activeClassName='nav-active'>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/add' activeClassName='nav-active'>
+                Add Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/about' activeClassName='nav-active'>
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 export default Header;
